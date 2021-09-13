99a4f12406f75916364d9a58473429e9e1b27f89
Augusto Souza <augustojsouza1@gmail.com>
2021-09-13 09:06:06 -0300

    Create User Intercepetor serializer

--- src/interceptors/serialize.interceptors.ts
+++ src/interceptors/serialize.interceptors.ts
@@ -7,15 +7,14 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
-import { nextTick } from 'process';
-
+import { UserDTO } from 'src/users/dtos/user.dto';
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    console.log('Before handler', context);
-
     return handler.handle().pipe(
       map((data: any) => {
-        console.log('Before response', data);
+        return plainToClass(UserDTO, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
